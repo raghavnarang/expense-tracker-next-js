808681ee7ddb50b6dfa1963e00e13cec707261c5
Raghav Narang <raghavvnarang@gmail.com>
2021-12-17 19:59:57 +0530

    first commit

---
+++ types/entry.ts
@@ -0,0 +1,22 @@
+type Entry = {
+    id: number,
+    groupId: number,
+    message: string,
+    amount: number,
+    createdAt: Date,
+    updatedAt: Date
+};
+
+export default Entry;
+
+export type EntryInput = {
+    message: string,
+    amount: number,
+    groupId: number
+}
+
+export type EditEntryInput = {
+    id: number, 
+    message: string,
+    amount: number,
+};
